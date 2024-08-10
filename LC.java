dda1a07c9c54fa516e5f725745d0ca642cfa469a
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-08-10 22:22:29 +0800

    group of anagrams

--- LC.java
+++ LC.java
@@ -38,4 +38,41 @@ public class LC{
         }
         return ans;
     }
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+        ArrayList<List<String>> res = new ArrayList<List<String>>();
+        for(int i = 0; i < strs.length; i++){
+            ArrayList<String> currList = new ArrayList<String>();
+            if(strs[i] == "AAA")
+                continue;
+            for(int j = i+1; j < strs.length; j++){
+                if(strs[j] != "AAA" && isAnagram(strs[i], strs[j])){
+                    currList.add(strs[j]);
+                    strs[j] = "AAA";
+                }
+            }
+            currList.add(strs[i]);
+            res.add(currList);
+            strs[i] = "AAA";
+        }
+        return res;
+    }
+
+    public boolean isAnagram(String str1, String str2){
+        if(str1.length() != str2.length())
+            return false;
+        int[] freq = new int[26];
+
+        for(int i = 0; i < str1.length(); i++){
+            freq[str1.charAt(i) - 'a']++;
+            freq[str2.charAt(i) - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0)
+                return false;
+        }
+        return true;
+    }
 }
