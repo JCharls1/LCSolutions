1b2d25d6c063511182a11082ca54d281cb446288
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-08-10 22:28:58 +0800

    two sum II

--- LC.java
+++ LC.java
@@ -75,4 +75,23 @@ public class LC{
         }
         return true;
     }
+
+    // two sum II
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length-1;
+        while(l < r){
+            int sum = numbers[l] + numbers[r];
+            if(sum == target){
+                int[] ans = new int[2];
+                ans[0] = l+1;
+                ans[1] = r+1;
+                return ans;
+            }
+            if(sum > target)
+                r--;
+            else
+                l++;
+        }
+        return new int[2];
+    }
 }
