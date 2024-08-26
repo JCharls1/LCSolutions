01126ed4ad86ee299ec53b356fc7085d599ae700
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-08-26 21:26:57 +0800

    jkhgh

--- LC.java
+++ LC.java
@@ -94,4 +94,25 @@ public class LC{
         }
         return new int[2];
     }
+    // valid parentheses
+    public List<String> generateParenthesis(int n) {
+        List<String> res = new ArrayList<String>();
+        recurse(res, 0, 0, "", n);
+        return res;
+    }
+    
+    public void recurse(List<String> res, int left, int right, String s, int n) {
+        if (s.length() == n * 2) {
+            res.add(s);
+            return;
+        }
+        
+        if (left < n) {
+            recurse(res, left + 1, right, s + "(", n);
+        }
+        
+        if (right < left) {
+            recurse(res, left, right + 1, s + ")", n);
+        }
+    }
 }
