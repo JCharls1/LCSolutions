2cf49b3b226b00229f116200e04625fe87757082
JCharls1 <151930727+JCharls1@users.noreply.github.com>
2024-09-12 21:07:52 +0800

    container with most water

--- LC.java
+++ LC.java
@@ -192,4 +192,24 @@ public class LC{
      * 1945. Sum of Digits of String After Convert
      */
 
+
+    /**11. Container with most water */
+    public int maxArea(int[] height) {
+        int left = 0, right = height.length-1;
+        int width = height.length-1;
+        int len = 0;
+        int ans = 0;
+        while(left <= right){
+            len = Math.min(height[left], height[right]);
+            ans = Math.max(ans, len*width);
+            width--;
+            if(height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        return ans;
+    }
+    /**11. Container with most water */
+    
 }
